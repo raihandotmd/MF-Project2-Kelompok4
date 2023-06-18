dd8e2fb0fb4a80f99a0d55f5f61c0223df374665
Raihan <muhraihan185@gmail.com>
2023-06-18 16:12:27 +0700

    add admin authentication middleware and role_id to user db

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+//use Illuminate\Auth\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// admin route
+Route::middleware(['admin.auth'])->group(function () {
+    Route::get('/admin', function () {
+        return 'admin page test';
+    })->name('admin');
+});
+
+// user route
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user', function () {
+        return 'user page test';
+    })->name('user');
+});
+
 Auth::routes();
+
+Route::get('/deny', function () {
+    return 'You are not allowed to access this page.';
+})->name('deny');
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
