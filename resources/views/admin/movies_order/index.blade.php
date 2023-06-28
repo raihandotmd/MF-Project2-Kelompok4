b1f5ee530c5fa281ff211e692d4c5167beb5ca70
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2023-06-28 19:08:55 +0700

    feat: movie order

--- resources/views/admin/movies_order/index.blade.php
+++ resources/views/admin/movies_order/index.blade.php
@@ -1,46 +1,54 @@
 @extends('admin.layouts.app')
 @section('content')
-    <h1 align="center">selamat datang di pesanan</h1>
-    <h1 class="mt-4">Tables</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-        <li class="breadcrumb-item active">Tables</li>
-    </ol>
-    <div class="card mb-4">
-        <div class="card-header">
-            <!-- <i class="fas fa-table me-1"></i> -->
-            <a class="btn btn-success" href="{{ url('/MovieOrder/store') }}">Create</a>
-        </div>
-        <div class="card-body">
-            <table id="datatablesSimple">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Movie schedule</th>
-                        <th>user</th>
-                        <th>seat</th>
-                        <th>ticket kode</th>
-                        <th>action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php $no = 1; @endphp
-                    @foreach ($movieOrders as $m)
-                        <tr>
-                            <td>{{ $no }}</td>
-                            <td>{{ $m->movie_schedule }}</td>
-                            <td>{{ $m->user_id }}</td>
-                            <td>{{ $m->seat }}</td>
-                            <td>{{ $m->ticket_code }}</td>
-                            <td>
-                                <a class="btn btn-primary" href=''>View</a>
-                                <a class="btn btn-primary" href=''>Edit</a>
-                                <a class="btn btn-primary" href=''>Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<section class="section">
+    <div class="row" id="table-hover-row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Movies Order Table</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body py-1">
+                        <a href="{{ route('movies_order.create') }}" class="btn btn-primary">Create</a>
+                    </div>
+                    <!-- table hover -->
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-0">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Movie schedule</th>
+                                    <th>user</th>
+                                    <th>seat</th>
+                                    <th>ticket kode</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($movies_order as $movie_order)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $movie_order->movie_schedule }}</td>
+                                    <td>{{ $movie_order->user_id }}</td>
+                                    <td>{{ $movie_order->seat }}</td>
+                                    <td>{{ $movie_order->ticket_code }}</td>
+                                    <td>
+                                        <a href="{{ route('movie_order.show', $movie_order->id) }}" class="btn btn-primary">Show</a>
+                                        <a href="{{ route('movie_order.edit', $movie_order->id) }}" class="btn btn-warning">Edit</a>
+                                        <form action="{{ route('movie_order.destroy', $movie_order->id) }}" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+</section>
 @endsection
