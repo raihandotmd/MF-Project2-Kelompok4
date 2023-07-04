df3b6d6b3bd04aac2c64c1d8cb059e20735e3b23
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2023-07-04 13:43:19 +0700

    feat: dev_frontend

--- resources/views/frontend/partials/contact.blade.php
+++ resources/views/frontend/partials/contact.blade.php
@@ -1,43 +1,43 @@
 <!-- contact section -->
 
 <section class="contact_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form_container">
-            <div class="heading_container">
-              <h2>
-                Contact Us
-              </h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form_container">
+          <div class="heading_container">
+            <h2>
+              Contact Us
+            </h2>
+          </div>
+          <form action="">
+            <div>
+              <input type="text" placeholder="Full Name " />
             </div>
-            <form action="">
-              <div>
-                <input type="text" placeholder="Full Name " />
-              </div>
-              <div>
-                <input type="email" placeholder="Email" />
-              </div>
-              <div>
-                <input type="text" placeholder="Phone number" />
-              </div>
-              <div>
-                <input type="text" class="message-box" placeholder="Message" />
-              </div>
-              <div class="d-flex ">
-                <button>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
+            <div>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <input type="text" placeholder="Phone number" />
+            </div>
+            <div>
+              <input type="text" class="message-box" placeholder="Message" />
+            </div>
+            <div class="d-flex ">
+              <button>
+                SEND
+              </button>
+            </div>
+          </form>
         </div>
-        <div class="col-md-6">
-          <div class="img-box">
-            <img src="images/contact-img.jpg" alt="">
-          </div>
+      </div>
+      <div class="col-md-6">
+        <div class="img-box">
+          <img src="images/contact-img.jpg" alt="">
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 
-  <!-- end contact section -->
+<!-- end contact section -->
