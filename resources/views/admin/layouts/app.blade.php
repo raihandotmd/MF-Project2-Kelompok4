318b20bf7fd1fd471b4eae7046717ab26daa213a
Raihan <muhraihan185@gmail.com>
2023-07-04 19:01:56 +0700

    fix: sidemenu button

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,7 +1,15 @@
 @include('admin.layouts.header')
 @include('admin.layouts.sidebar')
-<main id="main" class="d-flex justify-content-between">
-    @yield('content')
+
+<main id="main" class="justify-content-between">
+    <div class="group-container">
+        <header class="mb-3">
+            <a href="#" class="burger-btn d-block d-xl-none">
+                <i class="bi bi-justify fs-3"></i>
+            </a>
+        </header>
+        @yield('content')
+    </div>
     <x-admin.footer />
 </main>
 @include('admin.layouts.footer')
