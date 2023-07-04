5d7ac2a23514a109a54022eefdeef0165c274180
Raihan <muhraihan185@gmail.com>
2023-07-04 12:06:58 +0700

    refactor: web route

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,11 @@ Route::get('/', function () {
 
 // admin route
 Route::middleware(['admin.auth'])->group(function () {
-    Route::get('/admin', function () {
-        return 'admin page test';
-    })->name('admin');
+    Route::prefix('/admin')->group(function () {
+        Route::get('/', function () {
+            return 'admin page test';
+        })->name('admin');
+    }); 
 });
 
 // user route
@@ -52,72 +54,73 @@ Route::get('/deny', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-// Route::get('/MovieOrder', [MovieOrderController::class, 'index'])->name('MovieOrder');
-// Route::get('/Users', [UsersController::class, 'index'])->name('users');
-// Route::get('/UsersRoles', [UsersRolesController::class, 'index'])->name('UsersRoles');
-// Route::get('/MovieOrder/create', [MovieOrderController::class, 'create']);
-// Route::get('/MovieOrder/store', [MovieOrderController::class, 'store']);
 
 
-// Route::get('/movieGenre', [MovieGenreController::class, 'index'])->name('MovieOrder');
 
+Route::prefix('/movies')->group(function(){
+    Route::get('/', [MoviesController::class, 'index'])->name('movies');
+    Route::get('/create', [MoviesController::class, 'create'])->name('movies.create');
+    Route::post('/store', [MoviesController::class, 'store'])->name('movies.store');
+    Route::get('/show/{id}', [MoviesController::class, 'show'])->name('movies.show');
+    Route::get('/edit/{id}', [MoviesController::class, 'edit'])->name('movies.edit');
+    Route::put('/update', [MoviesController::class, 'update'])->name('movies.update');
+    Route::delete('/delete/{id}', [MoviesController::class, 'destroy'])->name('movies.destroy');
 
-Route::prefix('/movies/order')->group(function(){
-    Route::get('/', [MovieOrderController::class, 'index'])->name('movie_order');
-    Route::get('/create', [MovieOrderController::class, 'create'])->name('movie_order.create');
-    Route::post('/store', [MovieOrderController::class, 'store'])->name('movie_order.store');
-    Route::get('/edit/{id}', [MovieOrderController::class, 'edit'])->name('movie_order.edit');
-    Route::put('/update', [MovieOrderController::class, 'update'])->name('movie_order.update');
-    Route::delete('/delete/{id}', [MovieOrderController::class, 'destroy'])->name('movie_order.destroy');
+    Route::prefix('/schedule')->group(function(){
+        Route::get('/', [MovieScheduleController::class, 'index'])->name('movie_schedule');
+        Route::get('/create', [MovieScheduleController::class, 'create'])->name('movie_schedule.create');
+        Route::post('/store', [MovieScheduleController::class, 'store'])->name('movie_schedule.store');
+        Route::get('/edit/{id}', [MovieScheduleController::class, 'edit'])->name('movie_schedule.edit');
+        Route::put('/update', [MovieScheduleController::class, 'update'])->name('movie_schedule.update');
+        Route::delete('/delete/{id}', [MovieScheduleController::class, 'destroy'])->name('movie_schedule.destroy');
+    });
+
+    Route::prefix('/genre')->group(function(){
+        Route::get('/', [MovieGenreController::class, 'index'])->name('movie_genre');
+        Route::get('/create', [MovieGenreController::class, 'create'])->name('movie_genre.create');
+        Route::post('/store', [MovieGenreController::class, 'store'])->name('movie_genre.store');
+        Route::get('/edit/{id}', [MovieGenreController::class, 'edit'])->name('movie_genre.edit');
+        Route::put('/update', [MovieGenreController::class, 'update'])->name('movie_genre.update');
+        Route::delete('/delete/{id}', [MovieGenreController::class, 'destroy'])->name('movie_genre.destroy');
+    });
+
+    Route::prefix('/order')->group(function(){
+        Route::get('/', [MovieOrderController::class, 'index'])->name('movie_order');
+        Route::get('/create', [MovieOrderController::class, 'create'])->name('movie_order.create');
+        Route::post('/store', [MovieOrderController::class, 'store'])->name('movie_order.store');
+        Route::get('/edit/{id}', [MovieOrderController::class, 'edit'])->name('movie_order.edit');
+        Route::put('/update', [MovieOrderController::class, 'update'])->name('movie_order.update');
+        Route::delete('/delete/{id}', [MovieOrderController::class, 'destroy'])->name('movie_order.destroy');
+    });
 });
 
-Route::prefix('/movies/genre')->group(function(){
-    Route::get('/', [MovieGenreController::class, 'index'])->name('movie_genre');
-    Route::get('/create', [MovieGenreController::class, 'create'])->name('movie_genre.create');
-    Route::post('/store', [MovieGenreController::class, 'store'])->name('movie_genre.store');
-    Route::get('/edit/{id}', [MovieGenreController::class, 'edit'])->name('movie_genre.edit');
-    Route::put('/update', [MovieGenreController::class, 'update'])->name('movie_genre.update');
-    Route::delete('/delete/{id}', [MovieGenreController::class, 'destroy'])->name('movie_genre.destroy');
+Route::prefix('/cinemas')->group(function(){
+    Route::get('/', [CinemasController::class, 'index'])->name('cinemas');
+    Route::get('/create', [CinemasController::class, 'create'])->name('cinemas.create');
+    Route::post('/store', [CinemasController::class, 'store'])->name('cinemas.store');
+    Route::get('/edit/{id}', [CinemasController::class, 'edit'])->name('cinemas.edit');
+    Route::put('/update', [CinemasController::class, 'update'])->name('cinemas.update');
+    Route::delete('/delete/{id}', [CinemasController::class, 'destroy'])->name('cinemas.destroy');
 });
 
-Route::get('/movies', [MoviesController::class,'index'])->name('movies');
-Route::get('/movies/create', [MoviesController::class,'create'])->name('movies.create');
-Route::post('/movies/store', [MoviesController::class,'store'])->name('movies.store');
-Route::get('/movies/show/{id}', [MoviesController::class,'show'])->name('movies.show');
-Route::get('/movies/edit/{id}', [MoviesController::class,'edit'])->name('movies.edit');
-Route::put('/movies/update', [MoviesController::class,'update'])->name('movies.update');
-Route::delete('/movies/delete/{id}', [MoviesController::class,'destroy'])->name('movies.destroy');
+Route::prefix('/seats')->group(function(){
+    Route::get('/', [SeatsController::class, 'index'])->name('seats');
+    Route::get('/create', [SeatsController::class, 'create'])->name('seats.create');
+    Route::post('/store', [SeatsController::class, 'store'])->name('seats.store');
+    Route::get('/edit/{id}', [SeatsController::class, 'edit'])->name('seats.edit');
+    Route::put('/update', [SeatsController::class, 'update'])->name('seats.update');
+    Route::delete('/delete/{id}', [SeatsController::class, 'destroy'])->name('seats.destroy');
+});
 
+Route::prefix('/users')->group(function(){
+    Route::get('/', [UsersController::class, 'index'])->name('users');
 
-Route::get('/movies/schedule', [MovieScheduleController::class, 'index'])->name('movie_schedule');
-Route::get('/movies/schedule/create', [MovieScheduleController::class, 'create'])->name('movie_schedule.create');
-Route::post('/movies/schedule/store', [MovieScheduleController::class, 'store'])->name('movie_schedule.store');
-Route::get('/movies/schedule/edit/{id}', [MovieScheduleController::class, 'edit'])->name('movie_schedule.edit');
-Route::put('/movies/schedule/update', [MovieScheduleController::class, 'update'])->name('movie_schedule.update');
-Route::delete('/movies/schedule/delete/{id}', [MovieScheduleController::class, 'destroy'])->name('movie_schedule.destroy');
-
-
-Route::get('/cinemas', [CinemasController::class, 'index'])->name('cinemas');
-Route::get('/cinemas/create', [CinemasController::class, 'create'])->name('cinemas.create');
-Route::post('/cinemas/store', [CinemasController::class, 'store'])->name('cinemas.store');
-Route::get('/cinemas/edit/{id}', [CinemasController::class, 'edit'])->name('cinemas.edit');
-Route::put('/cinemas/update', [CinemasController::class, 'update'])->name('cinemas.update');
-Route::delete('/cinemas/delete/{id}', [CinemasController::class, 'destroy'])->name('cinemas.destroy');
-
-Route::get('/seats', [SeatsController::class, 'index'])->name('seats');
-Route::get('/seats/create', [SeatsController::class, 'create'])->name('seats.create');
-Route::post('/seats/store', [SeatsController::class, 'store'])->name('seats.store');
-Route::get('/seats/edit/{id}', [SeatsController::class, 'edit'])->name('seats.edit');
-Route::put('/seats/update', [SeatsController::class, 'update'])->name('seats.update');
-Route::delete('/seats/delete/{id}', [SeatsController::class, 'destroy'])->name('seats.destroy');
-
-Route::get('/users', [UsersController::class, 'index'])->name('users');
-Route::delete('/users/delete/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
-
-Route::get('/users/roles', [UsersRolesController::class, 'index'])->name('users_roles');
-Route::get('/users/roles/create', [UsersRolesController::class, 'create'])->name('users_roles.create');
-Route::post('/users/roles/store', [UsersRolesController::class, 'store'])->name('users_roles.store');
-Route::get('/users/roles/edit/{id}', [UsersRolesController::class, 'edit'])->name('users_roles.edit');
-Route::put('/users/roles/update', [UsersRolesController::class, 'update'])->name('users_roles.update');
-Route::delete('/users/roles/delete/{id}', [UsersRolesController::class, 'destroy'])->name('users_roles.destroy');
-
+    Route::prefix('/roles')->group(function(){
+        Route::get('/', [UsersRolesController::class, 'index'])->name('users_roles');
+        Route::get('/create', [UsersRolesController::class, 'create'])->name('users_roles.create');
+        Route::post('/store', [UsersRolesController::class, 'store'])->name('users_roles.store');
+        Route::get('/edit/{id}', [UsersRolesController::class, 'edit'])->name('users_roles.edit');
+        Route::put('/update', [UsersRolesController::class, 'update'])->name('users_roles.update');
+        Route::delete('/delete/{id}', [UsersRolesController::class, 'destroy'])->name('users_roles.destroy');
+    });
+});
