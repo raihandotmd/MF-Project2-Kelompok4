e962cba371cabd04ca172457c2b016e5ce311dba
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 01:24:07 +0700

    remove dashboard menu on admin page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -61,12 +61,7 @@
             <ul class="menu">
                 <li class="sidebar-title">Menu</li>
 
-                <li class="sidebar-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
-                    <a href="{{ route('dashboard') }}" class="sidebar-link">
-                        <i class="bi bi-grid-fill"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
+
 
                 <li class="sidebar-item has-sub {{ $isMoviesActive ? 'active' : '' }}">
                     <a href="#" class="sidebar-link">
