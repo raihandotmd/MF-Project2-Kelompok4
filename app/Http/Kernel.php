215269bf758e10ca338313954e9a8d1a58b7db65
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 17:10:44 +0700

    add role middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+        'role' => \App\Http\Middleware\Role::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
