ba165c19ad8bdf2a07cd0a856f7cb1a7dc3db6ce
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 17:14:42 +0700

    link logo to frontend

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -22,7 +22,8 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="index.html"><img src="./assets/compiled/svg/logo.svg" alt="Logo" srcset="" /></a>
+                    <a href="{{ route('frontend') }}"><img src="./assets/compiled/svg/logo.svg" alt="MoFlix"
+                            srcset="" /></a>
                 </div>
                 <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
