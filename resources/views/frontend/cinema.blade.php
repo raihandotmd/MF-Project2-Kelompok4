d4a6eb98d37e19b4d60990f7472622e761d1b7e6
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 11:59:51 +0700

    feat: add modal to cinema page

--- resources/views/frontend/cinema.blade.php
+++ resources/views/frontend/cinema.blade.php
@@ -9,7 +9,7 @@
                         <div class="container">
                             <div class="heading_container align-items-center">
                                 <h3 class="gt-teks pb-2">
-                                    Cinema Seats
+                                    Cinema {{ $cinema->code }} Seats
                                 </h3>
                                 <h4>
                                     pick one seat.
@@ -47,14 +47,99 @@
                                 <input type="hidden" name="selected_seat" id="selected-seat-input">
                                 <div class="text-center my-4">
                                     <button type="button" class="btn btn-danger" onclick="#">Cancel</button>
-                                    <button type="submit" class="btn-box rounded p-2" id="checkout-btn"
-                                        disabled>Checkout</button>
+                                    <button type="button" class="btn-box rounded p-2" id="checkout-btn"
+                                        data-bs-target="#confirmModal" data-bs-toggle="modal" disabled>Next</button>
                                 </div>
                             </form>
+                            <!-- Button trigger modal -->
 
+                            <!-- Modal -->
+                            <div class="modal fade" id="confirmModal" tabindex="-1" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm Your Ticket
+                                                Order</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="table-responsive">
+                                                <table class="table table-striped">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th scope="row">Movie</th>
+                                                            <td>{{ \App\Models\Movies::find($movie->movie_id)->title }}
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th scope="row">Starts</th>
+                                                            <td>{{ $movie->starts }}
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th scope="row">Ends</th>
+                                                            <td>{{ $movie->ends }}
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th scope="row">Seat</th>
+                                                            <td id="selected-seat"></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th scope="row">Price</th>
+                                                            <td>Rp. {{ $movie->price }}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn-box rounded p-2"
+                                                data-bs-target="#qrCodeModal" data-bs-toggle="modal">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Modal 2 -->
+                            <div class="modal fade" id="qrCodeModal" tabindex="-1" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Qris Payment</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img src="{{ asset('assets/images/qrframe.png') }}" alt="Qr Payment">
+                                            <p class="text-center">Scan this QR Code to pay Rp. {{ $movie->price }}</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <a href="#" class="btn-primary">Check Transaction</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <script>
                                 const seats = document.querySelectorAll('.seat');
                                 let selectedSeat = null;
+
+                                // Retrieve selected seat from localStorage
+                                const storedSeat = localStorage.getItem('selectedSeat');
+                                if (storedSeat) {
+                                    const seat = document.querySelector(`.seat[data-id="${storedSeat}"]`);
+                                    if (seat) {
+                                        seat.classList.add('selected');
+                                        selectedSeat = seat;
+                                        document.getElementById('selected-seat-input').value = seat.textContent.trim();
+                                        document.getElementById('selected-seat').textContent = seat.textContent.trim();
+                                        document.getElementById('checkout-btn').disabled = false;
+                                    }
+                                }
 
                                 seats.forEach(seat => {
                                     seat.addEventListener('click', () => {
@@ -65,6 +150,10 @@
                                         selectedSeat = seat;
                                         document.getElementById('checkout-btn').disabled = false;
                                         document.getElementById('selected-seat-input').value = seat.textContent.trim();
+                                        document.getElementById('selected-seat').textContent = seat.textContent.trim();
+
+                                        // Store selected seat in localStorage
+                                        localStorage.setItem('selectedSeat', seat.dataset.id);
                                     });
                                 });
                             </script>
