eeef9f162948e5fa4c93155482ba77a7e5e98b14
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 17:09:57 +0700

    change default user role  id

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role_id' => 1,
+            'role_id' => 2,
         ]);
     }
 }
