22def97979f8e5f3eff1692f19d35da8ae083ea1
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 22:37:52 +0700

    remove image src

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -22,8 +22,7 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="{{ route('frontend') }}"><img src="./assets/compiled/svg/logo.svg" alt="MoFlix"
-                            srcset="" /></a>
+                    <a href="{{ route('frontend') }}"><img src="" alt="MoFlix" srcset="" /></a>
                 </div>
                 <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
