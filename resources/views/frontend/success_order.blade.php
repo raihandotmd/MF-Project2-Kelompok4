1a1db98aadf1d8c0aca6295007011cec194d7769
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 12:47:13 +0700

    feat: success order page

---
+++ resources/views/frontend/success_order.blade.php
@@ -0,0 +1,19 @@
+@include('frontend.partials._header')
+
+<div class="d-flex justify-content-center align-items-center bg-light" style="min-height: 100vh;">
+    <div class="card p-3">
+        <div class="card-body text-center ">
+            <div class="text-success">
+                <i class="fa fa-check" aria-hidden="true" style="font-size: 500%"></i>
+            </div>
+            <h1 class="card-title fw-bold text-success">Success</h1>
+            <p class="card-text text-secondary">We received your order request!</p>
+
+            <p class="card-text mt-2">Here is your Tickets: <span class="fw-bold">#asdasdasdasda</span></p>
+            <div class="a-link mt-3 d-flex align-items-center gap-2 justify-content-center">
+                <a href="" class="btn btn-danger">Back to Home</a>
+                <a href="" class="view rounded">View Order</a>
+            </div>
+        </div>
+    </div>
+</div>
