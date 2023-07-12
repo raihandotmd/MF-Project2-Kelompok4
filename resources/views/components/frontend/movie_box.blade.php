962bf05717aafef6c990d848a289f21e7e8502e2
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-07-12 07:16:22 +0700

    change the new div to price

--- resources/views/components/frontend/movie_box.blade.php
+++ resources/views/components/frontend/movie_box.blade.php
@@ -1,28 +1,28 @@
 @props(['movie'])
 
 @php
-    $movie = \App\Models\Movies::find($movie->movie_id);
+    $movieSchedule = \App\Models\Movies::find($movie->movie_id);
 @endphp
 
 <div class="box">
-    <a href="{{ route('movie.show', $movie->id) }}">
+    <a href="{{ route('movie.show', $movieSchedule->id) }}">
         <div class="img-box px-2">
-            <img src="{{ asset('storage/' . $movie->image) }}" alt="{{ $movie->title }}">
+            <img src="{{ asset('storage/' . $movieSchedule->image) }}" alt="{{ $movieSchedule->title }}">
         </div>
         <div class="detail-box">
             <h6>
-                {{ $movie->genre->name }}
+                {{ $movieSchedule->genre->name }}
             </h6>
             <h6>
                 Rating:
                 <span>
-                    {{ $movie->rating }}/5
+                    {{ $movieSchedule->rating }}/5
                 </span>
             </h6>
         </div>
         <div class="new">
             <span>
-                TOP MOVIES
+                Rp. {{ $movie->price }}
             </span>
         </div>
     </a>
